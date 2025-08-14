866cfa688574e4cfce6c7f86ba334b2b7240c28a
Ayush Jasrotia(UST <299773@ust.com>
2025-08-13 20:40:46 -0700

    Revert "dns fix3"
    This reverts commit 025f911421ce95fa1aa3a4a974808df86fd303b1.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const API_URL = "http://18.234.69.130:32000"; // Change if backend is hosted elsewhere
+const API_URL = "http://18.234.69.130:5000"; // Change if backend is hosted elsewhere
 
 function App() {
   const [notes, setNotes] = useState([]);
